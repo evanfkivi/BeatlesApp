be6ae4e26038f3b79d2c6d2ca48e0a6308e73f9a
Evan Kivi <evanfkivi@gmail.com>
2025-10-11 20:30:18 -0500

    get rid of this comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // interceptor
-//    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     implementation("io.coil-kt:coil-compose:2.6.0")
