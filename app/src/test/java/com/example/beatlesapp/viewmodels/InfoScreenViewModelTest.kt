cd5703c6df941582dc7ea7f3ea276ec42c29c7d1
Evan <evgancodking@gmail.com>
2026-01-13 21:26:39 -0600

    Trying to create similar InfoScreenViewModelTest.kt tests but savestatehandle is broken again ):

--- app/src/test/java/com/example/beatlesapp/viewmodels/InfoScreenViewModelTest.kt
+++ app/src/test/java/com/example/beatlesapp/viewmodels/InfoScreenViewModelTest.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.SavedStateHandle
 import com.example.beatlesapp.model.Routes
 import com.example.beatlesapp.network.FakeNetworkBeatlesRepository
 import com.example.beatlesapp.network.FakeNetworkResults
+import com.example.beatlesapp.ui.screens.BeatlesUiState
+import com.example.beatlesapp.ui.screens.BeatlesViewModel
 import com.example.beatlesapp.ui.screens.InfoScreenViewModel
 import com.example.beatlesapp.ui.screens.InfoUiState
 import junit.framework.TestCase.assertTrue
@@ -12,6 +14,7 @@ import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
+import java.io.IOException
 import kotlin.test.assertEquals
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -46,4 +49,53 @@ class InfoScreenViewModelTest {
 
             assertEquals(FakeNetworkResults.fakeGetReleaseDetails, success.details)
         }
+
+//    @Test
+//    fun InfoScreenViewModel_getAlbum_verifyError() =
+//        runTest{
+//            val savedState = SavedStateHandle(
+//                mapOf("route" to Routes.Info(0))
+//            )
+//
+//            val repository = FakeNetworkBeatlesRepository(
+//                throwable = IOException("IOException")
+//            )
+//
+//            val viewModel = InfoScreenViewModel(
+//                beatlesRepository = repository,
+//                savedStateHandle = savedState
+//            )
+//
+//            advanceUntilIdle()
+//
+//            val state = viewModel.infoUiState
+//
+//            kotlin.test.assertTrue(state is InfoUiState.Error)
+//        }
+//
+//    @Test
+//    fun InfoScreenViewModel_getAlbum_verifyIOException() =
+//        runTest{
+//            val savedState = SavedStateHandle(
+//                mapOf("route" to Routes.Info(0))
+//            )
+//
+//            val repository = FakeNetworkBeatlesRepository(
+//                throwable = IOException("IOException")
+//            )
+//
+//            val viewModel = InfoScreenViewModel(
+//                beatlesRepository = repository,
+//                savedStateHandle = savedState
+//            )
+//
+//            advanceUntilIdle()
+//
+//            val state = viewModel.infoUiState
+//
+//            assertEquals(
+//                "IOException",
+//                (state as InfoUiState.Error).message
+//            )
+//        }
 }
